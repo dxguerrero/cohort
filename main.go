fdafc9734965d708586955b0b61baefacea0e042
Daniel Guerrero <danxguerrero@google.com>
2023-11-09 16:29:13 -0500

    Added GET by name, DELETE and PUT routes

--- main.go
+++ main.go
@@ -37,7 +37,10 @@ var swes = []swe{
 func main() {
 	router := gin.Default()
 	router.GET("/swes", getSwes)
+	router.GET("/swes/:name", getSweByName)
 	router.POST("/swes", postSwe)
+	router.DELETE("/swes/:name", deleteSWE)
+	router.PUT("/swes/:name", updateSwe)
 
 	router.Run("localhost:8080")
 }
@@ -51,7 +54,7 @@ func getSwes(c *gin.Context) {
 func postSwe(c *gin.Context) {
 	var newSwe swe
 
-	//Call BindJSON to bing the received JSON to newSWE
+	//Call BindJSON to bind the received JSON to newSWE
 	if err := c.BindJSON(&newSwe); err != nil {
 		return
 	} 
@@ -61,7 +64,61 @@ func postSwe(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newSwe)
 }
 
+// getSweByName locates SWE apprentice whose name value matches
+// the name parameter sent by the client, then returns that SWE 
+// as a response
+func getSweByName(c *gin.Context) {
+	name := c.Param("name")
 
+	// Loop over the list of SWEs, looking for 
+	// a SWE whos Name value matches the parameter
+	for _, s := range swes {
+		if s.Name == name {
+			c.IndentedJSON(http.StatusOK, s)
+			return
+		}
+	}
 
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "SWE not found"})
+}
 
+// deleteSwe locates a SWE apprentice whose name value matches 
+// the name parameter sent by the client, and removes it from the
+// swes slice
+func deleteSWE(c *gin.Context) {
+	name := c.Param("name")
 
+	// Loop over the list of SWES, looking for
+	// a SWE whos Name value matches the parameter
+	for i, s := range swes {
+		if s.Name == name {
+			swes = append(swes[:i], swes[i+1:]...)
+			c.IndentedJSON(http.StatusOK, gin.H{"message": "User deleted successfully."})
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "SWE not found"})
+}
+
+// updateSwe locates SWE whose name value matches the name
+// parameter sent by the client, and updates it with the data
+// passed into request
+func updateSwe(c *gin.Context) {
+	name := c.Param("name")
+	var updatedSwe swe 
+	
+	if err := c.BindJSON(&updatedSwe); err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "There was an error updating the SWE"})
+	}
+
+	for i, s := range swes {
+		if s.Name == name {
+			swes[i] = updatedSwe
+			c.IndentedJSON(http.StatusOK, swes[i])
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "SWE not found"})
+}
