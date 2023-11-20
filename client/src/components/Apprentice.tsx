7f9b2041da4c769f7ede67bb769d3f20197c0095
Daniel Guerrero <danxguerrero@google.com>
2023-11-19 23:48:01 -0500

    Added styling to list view

--- client/src/components/Apprentice.tsx
+++ client/src/components/Apprentice.tsx
@@ -1,30 +1,49 @@
-import React from 'react';
+import React from "react";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button"
 
 type ApprenticeProps = {
-    apprentice: {
-        Name: string
-        Language: string
-        Hub: string
-        Img: string
-    }
-    setAreApprenticesVisible: (b: boolean) => void;
-    fetchApprentice: (name:string) =>  void;
-}
+  apprentice: {
+    Name: string;
+    Language: string;
+    Hub: string;
+    Img: string;
+  };
+  setAreApprenticesVisible: (b: boolean) => void;
+  fetchApprentice: (name: string) => void;
+};
 
-export const Apprentice = ({ apprentice, setAreApprenticesVisible, fetchApprentice }: ApprenticeProps) => {
-    async function handleClick(apprentice: {}): Promise<any> {
-        await fetchApprentice(apprentice.Name);
-        setAreApprenticesVisible(false);
-    }
+export const Apprentice = ({
+  apprentice,
+  setAreApprenticesVisible,
+  fetchApprentice,
+}: ApprenticeProps) => {
+  async function handleClick(apprentice: {}): Promise<any> {
+    await fetchApprentice(apprentice.Name);
+    setAreApprenticesVisible(false);
+  }
 
-    return (
-        <div className='apprentice-card'>
-            <div className='circle'>
-                <img className="list-img" src={apprentice.Img}/>
-            </div>
-            <h3>{apprentice.Name}</h3>
-            <p>{apprentice.Hub}</p>
-            <button onClick={() => handleClick(apprentice)}>View Apprentice</button>
-        </div>
-    )
-}
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Image src={apprentice.Img} width={150} height={150} roundedCircle />
+        </Col>
+        <Col>
+          <h3>{apprentice.Name}</h3>
+        </Col>
+        <Col>
+          <p>{apprentice.Hub}</p>
+        </Col>
+        <Col>
+          <Button onClick={() => handleClick(apprentice)}>
+            View Apprentice
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
